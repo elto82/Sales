7dd108bc8b0578632b691382428394aade1fddf7
elto82 <elto.82@gmail.com>
2018-08-18 00:16:13 -0500

    creacion de proyecto

---
+++ Sales.Backen/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sales.Backen.MvcApplication" Language="C#" %>
---
+++ Sales.Backen/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sales.Backen.Startup))]
+namespace Sales.Backen
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
